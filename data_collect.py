5f99cb783caabfa7f79a5acb934d69332ff0a963
Michael Allen <mallenjr42@gmail.com>
2021-11-03 17:29:55 -0500

    created new dataset and model

---
+++ data_collect.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+# initialize mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+
+# thumbs = open('thumbs.dat', 'wb')
+
+count = 0
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read each frame from the webcam
+    _, image = cap.read()
+
+    x, y, c = image.shape
+
+    # Flip the frame vertically
+    image = cv2.flip(image, 1)
+    imagergb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    cv2.imshow("raw", imagergb)
+
+    # Get hand landmark prediction
+    result = hands.process(imagergb)
+    
+    className = ''
+
+    landmarks = []
+
+    # post process the result
+    if result.multi_hand_landmarks:
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # print(id, lm)
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+
+                landmarks.append([lmx, lmy])
+
+            # Drawing landmarks on frames
+            mpDraw.draw_landmarks(image, handslms, mpHands.HAND_CONNECTIONS)
+
+    cv2.putText(image, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+
+    # Show the final output
+    cv2.imshow("Output", image) 
+
+    key = cv2.waitKey(1)
+    
+    if key == 48 and len(landmarks) > 0:
+      print('save fist - count ', count)
+      count += 1
+    elif key == 49 and len(landmarks) > 0:
+      print('save stop - count ', count)
+      count += 1
+    elif key == 50 and len(landmarks) > 0:
+      print('save peace - count ', count)
+      count += 1
+    elif key == 51 and len(landmarks) > 0:
+      print('save thumbs - count ', count)
+      count += 1
+      # np.save(thumbs, np.array(landmarks))
+    elif key == ord('q'):
+      break
+
+# release the webcam and destroy all active windows
+cap.release()
+
+cv2.destroyAllWindows()
---
+++ model_train.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+
+
+train_data = []
+train_labels = []
+
+test_data = []
+test_labels = []
+
+def load_data(name, label):
+  saltue = open('data/' + name + '.dat', 'rb')
+
+  for i in range(0, 250):
+    train_data.append(np.load(saltue).tolist())
+    train_labels.append(label)
+
+  for i in range (250, 300):
+    test_data.append(np.load(saltue).tolist())
+    test_labels.append(label)
+
+
+model = Sequential()
+model.add(Flatten())
+model.add(Dense(20, input_dim = 42, activation = 'relu'))
+model.add(Dense(15, activation = 'relu'))
+model.add(Dense(5, activation = 'softmax'))
+
+  
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+
+
+load_data('fist', 0)
+load_data('stop', 1)
+load_data('peace', 2)
+load_data('thumbsup', 3)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+
+model.fit(train_data, train_labels, epochs=10, shuffle=True, batch_size=10)
+
+model.save('./gesture_model_new')
+
+model.evaluate(test_data, test_labels, verbose=1)
